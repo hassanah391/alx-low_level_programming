d01d8898c53cd26c2be377cfa5f53322fb250a49
Hassan Ahmed <hassan.ahmed3577@gmail.com>
2024-06-22 22:00:52 +0300

    Ubdated a c file called 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,12 +3,13 @@
 /**
  * main - Entry point of the program
  *
- * Description: A simple program that prints "with proper grammar, but the outcome is a piece of art," using the printf function.
+ * Description: A simple program that prints "with proper grammar, but the
+ *outcome is a piece of art," using the printf function.
  *
  * Return: 0 if successful, non-zero otherwise
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art, \n");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
