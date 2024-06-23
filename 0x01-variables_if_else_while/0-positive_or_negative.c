8c671b91cbc607aa8492b3eeebf585ae026b495c
Hassan Ahmed <hassan.ahmed3577@gmail.com>
2024-06-23 20:44:04 +0300

    Made 0-positive_or_negative.c follows betty

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,7 +2,12 @@
 #include <time.h>
 #include <stdio.h>
 
-/* betty style doc for function main goes there */
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints whether the variable n is negative, positive, or zero.
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
