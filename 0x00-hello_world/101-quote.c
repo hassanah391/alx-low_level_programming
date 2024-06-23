6273ee41c996593c67881ec70f7797b342b3a7b3
Hassan Ahmed <hassan.ahmed3577@gmail.com>
2024-06-23 08:26:32 +0300

    Updated a c file called 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,16 +2,15 @@
 #include <string.h>
 
 /**
- * main : is entry point for the program.
- * Description: This program prints "and that piece of art is useful"
- * - Dora Korpar, 2015-10-19" , followed by a new line, to the standard error
- * without using any function in manual page 3
- * Return: value is 1
+ * main - Prints a specific message to the standard error
+ *
+ * Return: Always 1
  */
 
 int main(void)
 {
-char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+char *message =
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 write(STDOUT_FILENO, message, strlen(message));
 return (1);
 }
