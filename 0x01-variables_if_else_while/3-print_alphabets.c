dc119f201e49ea02f3c66ed3bb06e1380c4c252f
Hassan Ahmed <hassan.ahmed3577@gmail.com>
2024-06-24 11:39:53 +0300

    Added 3-print_alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints the alphabit in lowercase.
+ *
+ * Return: Always 0 (Success).
+ */
+int main(void)
+{
+        char i;
+
+        for (i = 'a'; i <= 'z'; i++)
+	{
+        putchar(i);
+	}
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+	putchar(i - 32);
+	}
+        putchar('\n');
+
+
+        return (0);
+}
