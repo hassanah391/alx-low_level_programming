2ccd9f3a8dfa6b8415f26ec7580864ded6f3fbc0
Hassan Ahmed <hassan.ahmed3577@gmail.com>
2024-06-24 20:13:56 +0300

    Updated 8-print_base16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -3,7 +3,8 @@
 /**
  * main - Entry point of the program
  *
- * Description: Print numbers from 0 to 9.
+ * Description: prints all the numbers of base 16 in lowercase,
+ *              followed by a new line.
  *
  * Return: Always 0 (Success).
  */
