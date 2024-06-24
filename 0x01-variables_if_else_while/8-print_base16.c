7d285df5d257f8f893b6a36fe696241172961eef
Hassan Ahmed <hassan.ahmed3577@gmail.com>
2024-06-24 20:10:41 +0300

    Added 8-print_base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Print numbers from 0 to 9.
+ *
+ * Return: Always 0 (Success).
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+		putchar(i);
+
+	for (i = 97; i <= 102; i++)
+		putchar(i);
+	putchar('\n');
+
+	return (0);
+}
