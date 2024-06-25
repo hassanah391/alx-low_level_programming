6e09866adb341e210d965b2913e5264911d51a36
Hassan Ahmed <hassan.ahmed3577@gmail.com>
2024-06-25 22:53:33 +0300

    Added 1-alphabet.c

--- 0x02-functions_nested_loops/functions.c
+++ 0x02-functions_nested_loops/functions.c
@@ -11,3 +11,20 @@ int _putchar(char c)
 {
     return (write(1, &c, 1));
 }
+
+/**
+ * print_alphabet - prints the alphabet, in lowercase, followed by a new line.
+ *
+ *
+ * Return: Always 0.
+ *
+ *
+ */
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z';i++ )
+	_putchar(i);
+	_putchar('\n');
+}
