afd51fb9d2ff046a2895dd268310e8a72beaeee6
Hassan Ahmed <hassan.ahmed3577@gmail.com>
2024-06-25 19:36:29 +0300

    Updated 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,15 +7,15 @@
  */
 int main(void)
 {
-    char str[] = "_putchar";
-    int i = 0;
+	char str[] = "_putchar";
+	int i = 0;
 
-    while (str[i] != '\0')
-    {
-        _putchar(str[i]);
-        i++;
-    }
-    _putchar('\n');
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
 
-    return (0);
+	return (0);
 }
