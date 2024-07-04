defad971220a4e0c9c7cf396331a415c774b0370
Hassan Ahmed <hassan.ahmed3577@gmail.com>
2024-07-04 19:30:19 +0300

    Added 101-crackme

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -10,28 +10,26 @@
 int main(void)
 {
     char password[12];
-    int i, sum, diff;
+    int i, sum;
 
     srand(time(NULL));
 
     /* Generate random characters for the password */
-    for (i = 0; i < 11; i++)
+    for (i = 0; i < 10; i++)
     {
         password[i] = rand() % 94 + 33; /* ASCII printable characters range */
     }
-    password[11] = '\0';
+
+    /* Set the last character to null terminator */
+    password[10] = '\0';
 
     /* Calculate the expected checksum */
     sum = 0;
-    for (i = 0; i < 11; i++)
+    for (i = 0; i < 10; i++)
     {
         sum += password[i];
     }
-    sum %= 63;
-
-    /* Adjust the last character to make the sum match 62 */
-    diff = 62 - sum;
-    password[10] += diff;
+    password[10] = (sum % 63) + 33; /* Adjust last character based on checksum */
 
     printf("%s\n", password);
 
