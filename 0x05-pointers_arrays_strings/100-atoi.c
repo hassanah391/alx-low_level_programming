1590cdfd67651754da7b9073f89504152fcb014d
Hassan Ahmed <hassan.ahmed3577@gmail.com>
2024-07-04 19:12:39 +0300

    Made 100-atoi.c follows betty style

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,31 +8,31 @@
  */
 int _atoi(char *s)
 {
-    int result = 0;
-    int sign = 1;
-    int found_num = 0;
+	int result = 0;
+	int sign = 1;
+	int found_num = 0;
 
-    while (*s)
-    {
-        if (*s == '-')
-        {
-            sign *= -1;
-        }
-        else if (*s == '+')
-        {
-            sign *= 1;
-        }
-        else if (*s >= '0' && *s <= '9')
-        {
-            found_num = 1;
-            result = result * 10 + (*s - '0');
-        }
-        else if (found_num)
-        {
-            break;
-        }
-        s++;
-    }
+	while (*s)
+	{
+		if (*s == '-')
+		{
+			sign *= -1;
+		}
+		else if (*s == '+')
+		{
+			sign *= 1;
+		}
+		else if (*s >= '0' && *s <= '9')
+		{
+			found_num = 1;
+			result = result * 10 + (*s - '0');
+		}
+		else if (found_num)
+		{
+			break;
+		}
+		s++;
+	}
 
-    return (result * sign);
+	return (result * sign);
 }
