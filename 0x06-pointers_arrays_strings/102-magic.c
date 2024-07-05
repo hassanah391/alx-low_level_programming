d06c8da15976596ee5b435002596fb0351443859
Hassan Ahmed <hassan.ahmed3577@gmail.com>
2024-07-05 16:49:40 +0300

    Added Advanced files

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -8,11 +8,8 @@ int main(void)
 
   a[2] = 1024;
   p = &n;
-
-  /* One line of code to achieve a[2] = 98 */
-  *(p + 5) = 98;  /* This line sets a[2] = 98 */
-
-  /* ...so that this prints 98\n */
+  *(p + 5) = 98;
+ 
   printf("a[2] = %d\n", a[2]);
   return (0);
 }
