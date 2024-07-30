2fde2fcb863f34d89e1a5b3b469a99bc2f725b0a
Hassan Ahmed <hassan.ahmed3577@gmail.com>
2024-07-30 15:51:25 +0300

    add doc in 1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdio.h>
 
 /**
  * init_dog - Initializes a variable of type struct dog
@@ -12,6 +13,11 @@
  */
 void init_dog(dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL)
+	{
+		printf("OK\n");
+		return;
+	}
 	(*d).name = name;
 	(*d).age = age;
 	(*d).owner = owner;
