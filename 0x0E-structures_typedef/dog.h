384f5d7631d1416761352c7c9fb67800d317e19e
Hassan Ahmed <hassan.ahmed3577@gmail.com>
2024-07-30 14:23:51 +0300

    Made dog.h follows betty style

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,15 +1,13 @@
+#ifndef HEADER
+#define HEADER
 #include "stdio.h"
 #include "stdlib.h"
 
 /**
- * struct dog - Structure for storing dog information
- * @name: A pointer to a string representing the name of the dog
- * @age: A float representing the age of the dog in years
- * @owner: A pointer to a string representing the name of the dog's owner
- *
- * Description: This structure stores basic information about a dog,
- * including its name, age, and owner's name. It is intended for use in
- * programs that need to manage and manipulate data related to dogs.
+ * struct dog - Structure to a dog
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
  */
 struct dog
 {
@@ -17,3 +15,5 @@ struct dog
 	float age;
 	char *owner;
 };
+
+#endif
