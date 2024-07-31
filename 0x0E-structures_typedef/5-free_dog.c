f7fdcb4eaccc105863b0dccd85b6abf3139263d5
Hassan Ahmed <hassan.ahmed3577@gmail.com>
2024-07-31 09:41:59 +0300

    Edited 5

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -4,12 +4,14 @@
 
 
 /**
- * free_dog: deallocates a dog structure
+ * free_dog - deallocates a dog structure
  * @d: a pointer points to the dog structure we want to deallocate
  *
  */
 void free_dog(dog_t *d)
 {
+	if (d == NULL)
+		return;
 
 	free(d->name);
 	free(d->owner);
