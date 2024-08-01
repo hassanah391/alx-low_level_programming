0513f92e5f0da104f6a71f1ebad0f539eb45179a
Hassan Ahmed <hassan.ahmed3577@gmail.com>
2024-08-01 10:57:17 +0300

    Made 0 follow betty style

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,8 +2,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * print_name - execute a function given as a parameter
+ * @name: parameter that function ptr needs
+ * @f:function to execute
+ *
+ */
 void print_name(char *name, void (*f)(char *))
 {
+	if (f == NULL)
+		return;
 
 	f(name);
 }
