35c2b896b40fb34ebd21067ffd66f4c7d3a5db2a
Hassan Ahmed <hassan.ahmed3577@gmail.com>
2024-08-15 04:21:45 +0300

    Added 2

--- 0x15-file_io/2-main.c
+++ 0x15-file_io/2-main.c
@@ -7,16 +7,11 @@
  *
  * Return: Always 0.
  */
-int main(int ac, char **av)
+int main()
 {
     int res;
 
-    if (ac != 3)
-    {
-        dprintf(2, "Usage: %s filename text\n", av[0]);
-        exit(1);
-    }
-    res = append_text_to_file(av[1], av[2]);
+    res = append_text_to_file(NULL, "Best School !");
     printf("-> %i)\n", res);
     return (0);
 }
