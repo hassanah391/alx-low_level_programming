355e40e90e58dc33c5feacf2cca2b378203f23ff
Hassan Ahmed <hassan.ahmed3577@gmail.com>
2025-01-31 21:04:57 +0200

    Updates the header

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -4,5 +4,6 @@
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 void print_array(int *array, int start, int end);
+int jump_search(int *array, size_t size, int value);
 
 #endif
