11c597d742c833ea895479a36ae87f37f19cc12f
Hassan Ahmed <hassan.ahmed3577@gmail.com>
2025-02-01 00:36:34 +0200

    Adds 102

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -6,5 +6,6 @@ int binary_search(int *array, size_t size, int value);
 void print_array(int *array, int start, int end);
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
 
 #endif
